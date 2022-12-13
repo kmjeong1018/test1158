04c86e9df9a8385aedfca736030fe989f9f9c583
정경민 <kmjeong@idevel.kr>
2022-12-13 22:06:34 +0900

    엑셀임포트

--- excellToOracle.php
+++ excellToOracle.php
@@ -1,10 +1,11 @@
-
 <?php
 $row = 1;
 $empty_sql = '';
+$char_sql = '';
 $cnt = 0;
 
 $empty_cnt = 0;
+$char_cnt = 0;
 $start = empty($_GET['start']) ? 0 : trim($_GET['start']);
 $end   = empty($_GET['end'])   ? 0 : trim($_GET['end']);
 ?>
@@ -12,36 +13,52 @@ $end   = empty($_GET['end'])   ? 0 : trim($_GET['end']);
 <body></body>
 <pre>
 <?php
+
+function date_check($str) { // 날자 포멧 체크
+  // 2022/11/01 (13:34)
+  
+  if ($str == 'NULL') return false;
+  else {
+    $str = str_replace("'", "", $str);
+    preg_match('/^(\d{4})\/(\d{2})\/(\d{2}) \((\d{2}):(\d{2})\)$/', $str, $match);
+    return (count($match) == 6);
+  }
+}
+
 if (!empty($start) && !empty($end) && ($handle = fopen("MakeshopMemberAgree202211.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 150, ",")) !== FALSE) {
 
-      if($row < $start) {
-        $row++;
-        continue;
-      }
-
-      if($row > $end) break; 
-
-      if($row == 1) {
-        $row++;
-        continue;
-      } 
-
+      // 변수정리
       $userId    = trim(str_replace(array('"', '='), '', $data[1]));
       $email     = trim($data[3]);
       $smsDate   = trim($data[4]);
       $emailDate = trim($data[6]);
       $smsDate   = empty($smsDate) ? 'NULL' : "'{$smsDate}'";
       $emailDate = empty($emailDate) ? 'NULL' : "'{$emailDate}'";
-      if (empty($userId)) {
+      
+      if($row < $start) {
+        $row++;
+
+      } else if($row > $end) {
+        break; 
+
+      } else if($row == 1) { // 엑셀 헤더 무시
+        $row++;
+
+      } else if(date_check($emailDate) == false || date_check($smsDate) == false) { // 하나라도 null 이면 스킵
+        $row++;
+        $char_cnt++;
+        $char_sql .= "\n{$email} \t {$userId} \t {$smsDate} \t {$emailDate}";
+        
+      } else if (empty($userId)) { // 아이디가 없다면 스킵
         $row++;
         $empty_cnt++;
         $empty_sql .= "\n{$email} \t {$userId} \t {$smsDate} \t {$emailDate}";
 
-      } else if(!empty($userId)) {
+      } else if(!empty($userId)) { 
         $row++;
         $cnt++;
-        $updateSql = "UPDATE TCUSTOMER SET SMS_YN_DATE = TO_DATE({$smsDate},'YYYY-MM-DD HH24:mi:ss'), EMAIL_YN_DATE = TO_DATE({$emailDate},'YYYY-MM-DD HH24:mi:ss') WHERE MEM_ID = '{$userId}';";
+        $updateSql = "UPDATE TCUSTOMER_20221213 SET SMS_YN_DATE = TO_DATE({$smsDate},'YYYY-MM-DD HH24:mi:ss'), EMAIL_YN_DATE = TO_DATE({$emailDate},'YYYY-MM-DD HH24:mi:ss') WHERE MEM_ID = '{$userId}';";
         echo "\n". $updateSql;
 
       }
@@ -52,6 +69,7 @@ if (!empty($start) && !empty($end) && ($handle = fopen("MakeshopMemberAgree20221
 }
 ?>
 </pre>
+
 <form name="frm" action="excellToOracle.php" method='get'> 
   <input type="text" name="start"  placeholder="start row number" value="<?=$start?>" />  ~   <input type="text" name="end"  placeholder="end row number" value="<?=$end?>" />
   <input type="submit" value="submit" onclick="this.form.submit();" />
@@ -60,11 +78,12 @@ if (!empty($start) && !empty($end) && ($handle = fopen("MakeshopMemberAgree20221
 <?php
 echo "
 <pre>
-  normal $cnt rows / empty $empty_cnt rows
+  normal $cnt rows / empty $empty_cnt rows / charactor $char_cnt rows
 </pre>";
 
 //echo "<br><br><br>  odd $odd_cnt rows <pre>". $odd_sql."</pre>";
 echo "<br>  empty $empty_cnt rows <pre>". $empty_sql."</pre>";
+echo "<br>  charactor $char_cnt rows <pre>". $char_sql."</pre>";
 
 
 // $tns = " 
